88a0352f62c4697a5c4a33599cfcfd5c78607d5d
makilic <42938485+makilic@users.noreply.github.com>
2021-03-28 03:45:07 +0300

    Add files via upload

--- home.js
+++ home.js
@@ -7,6 +7,7 @@ app.use(function(req, res, next) {
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
         console.log(xhttp.responseText)
+
     if (this.readyState == 4 && this.status == 200) {
        // Typical action to be performed when the document is ready:
        document.getElementById("demo").innerHTML = xhttp.responseText;
@@ -14,5 +15,5 @@ xhttp.onreadystatechange = function() {
       console.log("error")
     }
 };
-xhttp.open("GET", "https://www.instagram.com/koddefterim1/?__a=1", true);
+xhttp.open("GET", "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJryijc9s0K4gRG9aU7SDTXdA&key=[YOURAPIKEY]", true);
 xhttp.send();
